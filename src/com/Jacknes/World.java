5dc95ca993c1e23744eabe0a21628bdbcabe3af5
Jacknes <jack.pascoe43@gmail.com>
2017-07-11 17:53:33 +1000

    Initial commit

---
+++ src/com/Jacknes/World.java
@@ -0,0 +1,24 @@
+package com.Jacknes;
+
+/**
+ * Created by jacknes on 11/7/17.
+ */
+public class World {
+    private int size;
+    private int difficulty;
+
+    public World(int size, int difficulty)
+    {
+        this.size = size;
+        this.difficulty = difficulty;
+        //TODO: Generate Two-dim array
+        //TODO: Populate array;
+    }
+
+
+    public void play()
+    {
+        //TODO: Implement play function to allow a player to move around.
+
+    }
+}
